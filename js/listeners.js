1aa331b198db05596f1a2d7544acf9d2b6947860
rebeckajohansson94 <rebeckaemmajohansson@gmail.com>
2025-03-23 18:06:24 +0100

    first commit

---
+++ js/listeners.js
@@ -0,0 +1,10 @@
+// Event Listeners
+
+btnOpen.addEventListener('click', lunchMenuDisplay);
+btnClose.addEventListener('click', lunchMenuHide);
+
+btn.addEventListener('click', toggleMenu);
+
+headingStarters.addEventListener('click', startersDisplay);
+headingMaincourse.addEventListener('click', maincourseDisplay);
+headingDesserts.addEventListener('click', dessertsDisplay);
